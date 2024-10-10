c00c8377bf5b5db64b8b3d5ba1e212b2c6b8faf5
Aysu Aşçı <[aysuacunmedya@gmail.com]>
2024-10-10 21:29:32 +0300

    supabase js dosyasında kullanıcı kaydı yapıldı

--- supabase.js
+++ supabase.js
@@ -4,3 +4,16 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Kullanıcı kaydı 
+export const signUpUser = async (email, password) => {
+  const { user, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    console.error('Kayıt hatası:', error.message);
+    return null; 
+  }
+  return user; 
+};
